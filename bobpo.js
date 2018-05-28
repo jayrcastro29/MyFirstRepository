090ad2bebb2f5f6ca07d2c2e71920d0a8558e3c1
jayrcastro29 <castroarmandojr29@gmail.com>
2018-05-28 21:56:26 +0800

    alphabetical

--- bobpo.js
+++ bobpo.js
@@ -11,6 +11,7 @@ var Num ={
             console.log(Num.counter(sibobobpo,varlength));
 
     },
+
     counter:function(a,b){
            var count = 0
             for(x= 0 ; x<b; x++)
@@ -30,6 +31,36 @@ var Num ={
             return count;
     }
 }
+
+var Substrings = {
+    LongestSub:function(){
+        var s = "azcbobobegghakl"
+        var sliced;
+        var temp = 0;
+        var start, end;
+        for(x=0; x<s.length; x++){
+             var counter = 1;
+             i=x;
+            while(s[i]<=s[i+1]){
+                if(s[x]<= s[x+1]){
+                    counter += 1;
+                    if(counter>temp){
+                    temp=counter;
+                    start = x;
+                    }
+                }
+            i++;
+            }
+        }
+        end = temp+ start;
+        sliced = s.slice(start,end)
+     console.log(sliced);
+    }
+}
+
+
+
+
 //dajfhakjfhsdkjhfkj
 //edited
 //edit
